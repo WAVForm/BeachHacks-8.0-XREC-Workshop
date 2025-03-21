ee61d337d6541041575948ea5c6f5f4ea74347ed
upgr4de03 <russellharral4@gmail.com>
2025-03-20 21:10:35 -0700

    game end collider
    YIPPIE

--- scenes/hitbox.gd
+++ scenes/hitbox.gd
@@ -1,11 +1,10 @@
 extends Area3D
 
-signal player_hit
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body):
-	if body.is_in_group("enemies"):
+	if body is Enemy:
 		print("💀 Player hit by enemy!")
-		emit_signal("player_hit")
+		get_tree().quit()
